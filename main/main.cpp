52d29eaa4e2a9930fb26e5a3f3993fb57cb9dca2
chcj <zhenjiechen@foxmail.com>
2024-12-31 17:59:14 +0800

    delete unused code

--- main/main.cpp
+++ main/main.cpp
@@ -89,19 +89,6 @@ extern "C" void DAP_Thread(void *pvParameters);
 
 extern "C" void ui_main(void);
 
-// extern "C" uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t *buffer, uint16_t reqlen)
-// {
-//     return 0;
-// }
-
-// extern "C" void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const *buffer, uint16_t bufsize)
-// {
-//     static uint8_t s_tx_buf[CFG_TUD_HID_EP_BUFSIZE];
-
-//     DAP_ProcessCommand(buffer, s_tx_buf);
-//     tud_hid_report(0, s_tx_buf, sizeof(s_tx_buf));
-// }
-
 static void disconnect_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
 {
     web_server_stop((httpd_handle_t *)arg);
@@ -111,20 +98,6 @@ static void connect_handler(void *arg, esp_event_base_t event_base, int32_t even
 {
     web_server_init((httpd_handle_t *)arg);
 }
-
-// static void led_init(){
-//     gpio_pad_select_gpio(GPIO_NUM_40);
-//     gpio_pad_select_gpio(GPIO_NUM_41);
-//     gpio_pad_select_gpio(GPIO_NUM_42);
-
-//     gpio_set_direction(GPIO_NUM_40, GPIO_MODE_OUTPUT);
-//     gpio_set_direction(GPIO_NUM_41, GPIO_MODE_OUTPUT);
-//     gpio_set_direction(GPIO_NUM_42, GPIO_MODE_OUTPUT);
-
-//     gpio_set_level(GPIO_NUM_40, 1);
-//     gpio_set_level(GPIO_NUM_41, 1);
-//     gpio_set_level(GPIO_NUM_42, 0);
-// }
 
 storage_status_t get_storage_status(void)
 {
@@ -234,28 +207,6 @@ static void lv_tick_task(void *arg)
     lv_tick_inc(LV_TICK_PERIOD_MS);
 }
 
-static esp_err_t s_example_read_file(const char *path)
-{
-    ESP_LOGI(TAG, "Reading file %s", path);
-    FILE *f = fopen(path, "r");
-    if (f == NULL) {
-        ESP_LOGE(TAG, "Failed to open file for reading");
-        return ESP_FAIL;
-    }
-    char line[EXAMPLE_MAX_CHAR_SIZE];
-    fgets(line, sizeof(line), f);
-    fclose(f);
-
-    // strip newline
-    char *pos = strchr(line, '\n');
-    if (pos) {
-        *pos = '\0';
-    }
-    ESP_LOGI(TAG, "Read from file: '%s'", line);
-
-    return ESP_OK;
-}
-
  extern "C" void app_main(void)
 {
     esp_err_t ret;
@@ -285,11 +236,6 @@ static esp_err_t s_example_read_file(const char *path)
     };
 
     host.slot = VSPI_HOST;
-
-    // ret = spi_bus_initialize((spi_host_device_t)host.slot, &bus_cfg, VSPI_HOST);
-    // if (ret != ESP_OK) {
-    //     ESP_LOGE(TAG, "Failed to initialize bus.");
-    // }    
 
     sdspi_device_config_t slot_config = SDSPI_DEVICE_CONFIG_DEFAULT();
     slot_config.gpio_cs = SDSPI_CS;
@@ -333,103 +279,16 @@ static esp_err_t s_example_read_file(const char *path)
         sdmmc_card_print_info(stdout, card);        
     }   
 
-    // const char *file_foo = "/data/foo.txt";
-    // ret = s_example_read_file(file_foo);
-    // if (ret != ESP_OK) {
-    //     M5.Power.powerOff();
-    // }    
-
-    // char line[128];
-    // char *device_filename;
-    // device_filename = "/data/hello/inner.txt";
-    // ESP_LOGI(TAG, "Opening file");
-    // FILE *f;    
-    // f = fopen(device_filename, "wb");
-    // if (f == NULL) {
-    //     ESP_LOGE(TAG, "Failed to open file for writing");
-    // }    
-    // else {
-    //     fprintf(f, "This is written by the device");
-    //     fclose(f);        
-    // }
-    // ESP_LOGI(TAG, "File written");
-    // Open file for reading
-    // ESP_LOGI(TAG, "Reading file");
-    // f = fopen("/data/algorithm/test.txt", "rb");
-    // if (f == NULL) {
-    //     ESP_LOGE(TAG, "Failed to open file for reading");
-    //     return;
-    // }
-    // fgets(line, sizeof(line), f);
-    // fclose(f);   
-    // // strip newline
-    // char *pos = strchr(line, '\n');
-    // if (pos) {
-    //     *pos = '\0';
-    // }
-    // ESP_LOGI(TAG, "Read from file: '%s'", line);     
-    // ESP_ERROR_CHECK(esp_netif_init());
-    // ESP_ERROR_CHECK(esp_event_loop_create_default());
-    // ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, connect_handler, &http_server));
-    // ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, disconnect_handler, &http_server));
-    // ESP_ERROR_CHECK(example_connect());
     wifi_init_softap();
-
-    // tinyusb_config_t tusb_cfg = {
-    //     .device_descriptor = NULL,
-    //     .string_descriptor = NULL,
-    //     .string_descriptor_count = 0,
-    //     .external_phy = false,
-    //     .configuration_descriptor = NULL,
-    //     .self_powered = false,
-    //     .vbus_monitor_io = 0};
-
-    // tinyusb_config_cdcacm_t acm_cfg = {
-    //     .usb_dev = TINYUSB_USBDEV_0,
-    //     .cdc_port = TINYUSB_CDC_ACM_0,
-    //     .rx_unread_buf_sz = 64,
-    //     .callback_rx = usb_cdc_send_to_uart, // the first way to register a callback
-    //     .callback_rx_wanted_char = NULL,
-    //     .callback_line_state_changed = NULL,
-    //     .callback_line_coding_changed = usb_cdc_set_line_codinig};
-
-    // led_init();
 
     DAP_Setup();
 
-    // ESP_LOGI(TAG, "USB initialization");
-
-    // ret = msc_dick_mount(CONFIG_TINYUSB_MSC_MOUNT_PATH);
-    // tusb_cfg.configuration_descriptor = get_configuration_descriptor(ret);
-    // tusb_cfg.string_descriptor_count = get_string_descriptor_count(ret);
-    // tusb_cfg.string_descriptor = get_string_descriptor(ret);
-    // tusb_cfg.device_descriptor = get_device_descriptor();
-
-    // ESP_ERROR_CHECK(tinyusb_driver_install(&tusb_cfg));
-    // ESP_ERROR_CHECK(tusb_cdc_acm_init(&acm_cfg));
-
     programmer_init();
-    // cdc_uart_init(UART_NUM_0, GPIO_NUM_2, GPIO_NUM_1, 115200);
-    // cdc_uart_register_rx_handler(CDC_UART_USB_HANDLER, usb_cdc_send_to_host, (void *)TINYUSB_CDC_ACM_0);
-    // cdc_uart_register_rx_handler(CDC_UART_WEB_HANDLER, web_send_to_clients, &http_server);
-    // ESP_LOGI(TAG, "USB initialization DONE");
-
-
-        // Specify the usbip server task
-// #if (USE_TCP_NETCONN == 1)
-//     xTaskCreate(tcp_netconn_task, "tcp_server", 4096, NULL, 14, NULL);
-// #else // BSD style
-//     xTaskCreate(tcp_server_task, "tcp_server", 4096, NULL, 14, NULL);
-// #endif
 
     // DAP handle task
     xTaskCreate(DAP_Thread, "DAP_Task", 2048, NULL, 10, &kDAPTaskHandle);
-
-    // buzz_init();
-    // ESP_ERROR_CHECK(power_measure_init());
     
     //ui main
-    // ui_main();
     lv_init();
     lv_port_disp_init(); 
     lv_port_indev_init();
